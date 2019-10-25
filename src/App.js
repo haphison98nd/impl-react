feb0e561e53e6a1df93d1393a66e39191033a00f
Hà Phi Sơn <35570775+haphison98nd@users.noreply.github.com>
2019-10-26 01:06:09 +0700

    update1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props){
+        super(props);
 
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        }
+
+    }
+
+    componentDidMount() {
+        fetch("https://api.github.com/users/hatrunguet98/repos")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                isLoaded: true,
+                items: result,
+                });
+            },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    render(){
+        const { error, isLoaded, items } = this.state;
+        if(error){
+            const { error, isLoaded, items } = this.state;
+        }
+        else if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
+        else{
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.name}>
+                            {item.full_name}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
+} 
 export default App;
